320be133439ec2b57bd522eeb75535c7a7cd2f10
Light Play <lightplay456@gmail.com>
2025-11-03 09:05:48 +0300

    feat: добавить библиотеку для логирования

---
+++ src/shared/core/Shared.Logging/LoggingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Shared.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static IHostBuilder AddLogger(this IHostBuilder builder)
+        {
+            builder.UseSerilog((context, services, configuration) =>
+            {
+                configuration
+                    .MinimumLevel.Information()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                    .MinimumLevel.Override("System", LogEventLevel.Warning);
+
+                configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithThreadId()
+                    .Enrich.WithProperty("ApplicationName", context.HostingEnvironment.ApplicationName)
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+
+                configuration.ReadFrom.Configuration(context.Configuration);
+
+                if (context.HostingEnvironment.IsDevelopment())
+                    configuration.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+            });
+
+            return builder;
+        }
+    }
+}
