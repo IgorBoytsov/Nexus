98c21b19b1d33ff602465ec1bd66e053ffabc65b
Light Play <lightplay456@gmail.com>
2026-01-04 16:48:47 +0300

    feat: добавить веб-проект Nexus.Account.Web
    - Инициализирован новый проект на базе ASP.NET Core MVC для возможности пользователям управлять своими данными.
    - Интегрирована поддержка TypeScript.
    - Проект зарегистрирован в общем решении Nexus.sln.

---
+++ src/apps/Identity/Nexus.Account.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nexus.Account.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/apps/Identity/Nexus.Account.Web/Scripts/app.ts
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const myButton = document.getElementById('btnClickMe') as HTMLButtonElement;
+
+    if (myButton) {
+        myButton.addEventListener('click', () => {
+            alert("Кнопка нажата, а данные взяли из TS!");
+            console.log('Логи в браузере');
+        })
+    }
+})
+
+function greet(name: string): void {
+    console.log(`Привет, ${name} Это тестовый вызом ТайпСкрипта!`)
+}
