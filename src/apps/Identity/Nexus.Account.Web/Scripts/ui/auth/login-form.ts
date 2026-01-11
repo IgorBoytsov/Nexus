05dc52697f3361dc51a4c85fff1baa18bd376ca7
Light Play <lightplay456@gmail.com>
2026-01-11 08:52:56 +0300

    feat(auth): реализовать страницу входа и Zero-Knowledge авторизацию
    - Добавлено представление Login.cshtml с использованием макета _AuthLayout.
    - Реализован скрипт login-form.ts для обработки процесса входа на клиенте.
    - Внедрен алгоритм Zero-Knowledge:
        1. Получение публичной инфо о шифровании (соль, зашифрованный DEK) по логину.
        2. Локальная деривация ключей (KEK, AuthHash) из пароля и соли.
        3. Локальное дешифрование DEK для последующего доступа к данным.
        4. Отправка AuthHash в BFF для аутентификации.
    - Добавлен редирект на другую страницу после успешной авторизации.

---
+++ src/apps/Identity/Nexus.Account.Web/Scripts/ui/auth/login-form.ts
@@ -0,0 +1,45 @@
+﻿import { SecureDataService } from "../../services/secure-data-service.js"
+import { AccountApiService } from "../../services/account-api-service.js";
+import { LoginUserRequest } from "../../Requests/login-user-request.js";
+import { UserMenegementApiService, PublicEncryptionInfoResponse } from "../../auth/user-menagement-api-service.js";
+
+const crypto = new SecureDataService();
+const authApiClient = new AccountApiService();
+const userApiClient = new UserMenegementApiService();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form') as HTMLFormElement;
+
+    if (form) {
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+
+            const formData = new FormData(form);
+            const login = formData.get('login') as string;
+            const password = formData.get('password') as string;
+
+            try {
+
+                const userPublicInfo = await userApiClient.getPublicEncryptionInfo(login);
+                const saltBytes = crypto.fromBase64(userPublicInfo.clientSalt);
+
+                const { kek, authHash } = await crypto.deriveKeysFromPassword(password, saltBytes); 
+
+                const dek = await crypto.decryptData<Uint8Array>(userPublicInfo.encryptedDek, kek);
+
+                const request: LoginUserRequest = {
+                    Login: login,
+                    Password: authHash
+                };
+
+                await authApiClient.login(request);
+
+                window.location.href = '/Home/Privacy';
+
+            } catch (error) {
+                console.error("Ошибка в процессе входа:", error);
+                alert(`Ошибка входа: ${error instanceof Error ? error.message : error}`);
+            }
+        });
+    }
+});
