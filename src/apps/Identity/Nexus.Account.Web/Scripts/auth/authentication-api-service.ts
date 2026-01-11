b037f779b660acafdca1f3be41e3737394cd8616
Light Play <lightplay456@gmail.com>
2026-01-11 08:41:05 +0300

    feat: добавить сервис AuthenticationApiService для аутентификации
    - Реализован метод login для отправки запросов на авторизацию.
    - Сервис интегрирован с BaseApiService для переиспользования логики запросов.
    - Настроен базовый URL для взаимодействия с микросервисом аутентификации.

---
+++ src/apps/Identity/Nexus.Account.Web/Scripts/auth/authentication-api-service.ts
@@ -0,0 +1,12 @@
+﻿import { BaseApiService } from "../services/base-api-service.js";
+import { LoginUserRequest } from "../Requests/login-user-request.js";
+
+export class AuthenticationApiService extends BaseApiService {
+    constructor() {
+        super("http://localhost:5093/api/auth");
+    }
+
+    async login(data: LoginUserRequest): Promise<void> {
+        return await this.post<void>("/login", data);
+    }
+}
