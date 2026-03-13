0254630cf858c679466f54cd9c392e1bed7af39f
Light Play <lightplay456@gmail.com>
2026-03-13 13:48:06 +0300

    refactor: весь бекенд проекта был перенесен в собственную папку

---
+++ backend/src/apps/Identity/Nexus.Account.Web/Services/Http/IUserMenagementClient.cs
@@ -0,0 +1,10 @@
+﻿using Quantropic.Toolkit.Results;
+using Rebout.Nexus.Contracts.UserManagement.V1;
+
+namespace Nexus.Account.Web.Services.Http
+{
+    public interface IUserMenagementClient
+    {
+        Task<Result<PublicEncryptionInfoResponse?>> GetPublicEncryptionInfo(string login);
+    }
+}
