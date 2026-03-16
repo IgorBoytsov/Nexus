f69b41b48fa540203aff4c978cd2cedbb7aa8acc
Light Play <lightplay456@gmail.com>
2026-03-16 15:45:35 +0300

    refactor(dotnet): замена protobuf реализации контрактов на чистые реализации через record

---
+++ contracts/src-dotnet/Rebout.Nexus.Contracts/UserManagement/v1/CreateStatusRequest.cs
@@ -0,0 +1,4 @@
+namespace Rebout.Nexus.Contracts.UserManagement.v1
+{
+    public record CreateStatusRequest(string Name);
+}
---
+++ contracts/src-dotnet/Rebout.Nexus.Contracts/UserManagement/v1/RegisterUserRequest.cs
@@ -0,0 +1,4 @@
+namespace Rebout.Nexus.Contracts.UserManagement.v1
+{
+    public record RegisterUserRequest(string Login, string UserName, string Verifier, string ClientSalt, string EncryptionAlgorithm, int Iterations, string KdfType, string Email, string? Phone, string? IdGender, string? IdCountry);
+}
