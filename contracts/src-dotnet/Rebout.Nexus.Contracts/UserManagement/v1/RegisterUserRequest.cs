bac80be63e5b268718ae3213f78d3c36ba8c6827
Light Play <lightplay456@gmail.com>
2026-03-16 16:01:25 +0300

    feat: добавлено поле string EncryptedDek в RegisterUserRequest и повышена версия библиотеки до 1.3.1

--- contracts/src-dotnet/Rebout.Nexus.Contracts/UserManagement/v1/RegisterUserRequest.cs
+++ contracts/src-dotnet/Rebout.Nexus.Contracts/UserManagement/v1/RegisterUserRequest.cs
@@ -1,4 +1,4 @@
 namespace Rebout.Nexus.Contracts.UserManagement.v1
 {
-    public record RegisterUserRequest(string Login, string UserName, string Verifier, string ClientSalt, string EncryptionAlgorithm, int Iterations, string KdfType, string Email, string? Phone, string? IdGender, string? IdCountry);
+    public record RegisterUserRequest(string Login, string UserName, string Verifier, string ClientSalt, string EncryptedDek, string EncryptionAlgorithm, int Iterations, string KdfType, string Email, string? Phone, string? IdGender, string? IdCountry);
 }
