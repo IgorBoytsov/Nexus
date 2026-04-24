cedc5b59d90c82f467230c5e9681c1a116425731
Light Play <lightplay456@gmail.com>
2026-04-25 01:17:02 +0300

    feat: init profile page

---
+++ web/nexus-ui/src/pages/profile/profile.page.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-profile-page',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './profile.page.html',
+})
+export class ProfilePage{
+    
+}
