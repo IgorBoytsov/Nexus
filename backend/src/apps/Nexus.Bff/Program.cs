56767ceec2e0b9d5bbff48d7c66b100388d8c4b2
Light Play <lightplay456@gmail.com>
2026-04-29 14:07:31 +0300

    feat: setting cookie for development and prod

--- backend/src/apps/Nexus.Bff/Program.cs
+++ backend/src/apps/Nexus.Bff/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddAuthorization();
 builder.Services.AddServices().AddHttpClients(builder.Configuration);
 
 builder.Services.AddCors(options =>
@@ -19,14 +20,32 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "ReboutId";
-        options.Cookie.HttpOnly = true;
-        options.Cookie.SameSite = SameSiteMode.Strict;
-        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    });
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "Crossdyne";
+            options.Cookie.Domain = null;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; 
+            options.Cookie.SameSite = SameSiteMode.Lax;
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromDays(7);
+        });
+}
+else
+{
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "Crossdyne";
+            options.Cookie.Domain = ".crossdyne.com"; 
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromDays(7);
+        });
+}
 
 var app = builder.Build();
 
@@ -35,10 +54,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
 app.UseCors("AllowLocalFrontend");
 app.UseAuthentication(); 
+app.UseAuthorization();  
 
 app.UseHttpsRedirection();
 app.MapEndpoints(Assembly.GetExecutingAssembly());
