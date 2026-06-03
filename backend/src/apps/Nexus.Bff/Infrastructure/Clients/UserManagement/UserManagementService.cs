611c18cf8b8d4dbbf4b1bc5ca5a3eeaaf8074703
Light Play <lightplay456@gmail.com>
2026-06-03 13:20:29 +0300

    refactor: метод GetPublicEncryptionInfo вынесен из основного файла UserManagementService в файл UserManagementService.CryptoInfo (частичный класс)

--- backend/src/apps/Nexus.Bff/Infrastructure/Clients/UserManagement/UserManagementService.cs
+++ backend/src/apps/Nexus.Bff/Infrastructure/Clients/UserManagement/UserManagementService.cs
@@ -1,6 +1,5 @@
 using System.Text.Json;
 using Crossdyne.Toolkit.Results;
-using Rebout.Nexus.Contracts.UserManagement.v1;
 using Microsoft.Extensions.Options;
 
 namespace Nexus.Bff.Infrastructure.Clients.UserManagement
@@ -51,26 +50,6 @@ namespace Nexus.Bff.Infrastructure.Clients.UserManagement
             catch
             {
                 return null;
-            }
-        }
-
-        public async Task<Result<PublicEncryptionInfoResponse?>> GetPublicEncryptionInfo(string login)
-        {
-            try
-            {
-                var response = await _httpClient.GetAsync($"api/v1/users/{login}/crypto/public");
-                
-                if (!response.IsSuccessStatusCode)
-                {
-                    var errors = await response.Content.ReadFromJsonAsync<Error[]>(_jsonOptions);
-                    return Result<PublicEncryptionInfoResponse?>.Failure(errors!);
-                }
-
-                return Result<PublicEncryptionInfoResponse?>.Success(await response.Content.ReadFromJsonAsync<PublicEncryptionInfoResponse>());
-            }
-            catch (Exception ex)
-            {
-                return Result<PublicEncryptionInfoResponse?>.Failure(new Error(ErrorCode.Server, $"Ошибка в Api: {ex}"));
             }
         }
     }
