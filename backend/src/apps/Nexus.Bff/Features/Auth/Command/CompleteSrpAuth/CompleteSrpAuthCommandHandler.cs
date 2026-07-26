ed6e5475aeba6d40a9fc8ae8ba324962be8e923a
Light Play <lightplay456@gmail.com>
2026-07-26 22:22:48 +0300

    fix: исправлено передаваемое значение в Redis с userSession.UserId на userSession.SessionId

--- backend/src/apps/Nexus.Bff/Features/Auth/Command/CompleteSrpAuth/CompleteSrpAuthCommandHandler.cs
+++ backend/src/apps/Nexus.Bff/Features/Auth/Command/CompleteSrpAuth/CompleteSrpAuthCommandHandler.cs
@@ -22,7 +22,7 @@ namespace Nexus.Bff.Features.Auth.Command.CompleteSrpAuth
             if (!resultCache)
                 return new Error(ErrorCode.Server, "Произошла непредвиденная ошибка на стороне сервера. Пожалуйста повторите процесс входа.");
 
-            await cache.SetAddAsync(RedisKeyExtensions.UserSessionsKey(userSession.UserId), userSession.UserId, TimeSpan.FromDays(30));
+            await cache.SetAddAsync(RedisKeyExtensions.UserSessionsKey(userSession.UserId), userSession.SessionId, TimeSpan.FromDays(30));
 
             await cache.RemoveAsync(templateCacheKey);
 
