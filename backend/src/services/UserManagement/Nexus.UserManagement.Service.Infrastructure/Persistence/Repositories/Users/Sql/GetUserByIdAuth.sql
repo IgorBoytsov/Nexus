3910413165bbd1c14c50be0342a25f07a20cc7d3
Light Play <lightplay456@gmail.com>
2026-08-05 00:20:49 +0300

    fix: исправлена проблема маппинга Dapper запроса в record тип

--- backend/src/services/UserManagement/Nexus.UserManagement.Service.Infrastructure/Persistence/Repositories/Users/Sql/GetUserByIdAuth.sql
+++ backend/src/services/UserManagement/Nexus.UserManagement.Service.Infrastructure/Persistence/Repositories/Users/Sql/GetUserByIdAuth.sql
@@ -6,6 +6,7 @@ SELECT
     ua.srp_encrypted_verifier as EncryptedVerifier,
     ua.srp_salt as ClientSalt,
     ua.srp_version as SrpVersion,
+    ua.srp_crypto_version as SrpCryptoVersion,
     ua."srp_encrypted_verifier_wrapKey" as EncryptedVerifierWrapKey,
     ua.srp_key_wrap_version as KeyWrapVersion,
     ua.srp_asymmetric_key_id as AsymmetricKeyId,
