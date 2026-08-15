b9773c4ab9b3e459ed31a845b9ec2ce0c6ce3894
Light Play <lightplay456@gmail.com>
2026-08-15 20:49:38 +0300

    fix: исправление теста ChangeEmailCommandHandlerTests

--- backend/tests/integration/Nexus.UserManagement.Service.Integration.Tests/Handlers/Commands/ChangeEmailCommandHandlerTests.cs
+++ backend/tests/integration/Nexus.UserManagement.Service.Integration.Tests/Handlers/Commands/ChangeEmailCommandHandlerTests.cs
@@ -9,6 +9,7 @@ using Nexus.UserManagement.Service.Domain.ValueObjects.User;
 using Nexus.UserManagement.Service.Infrastructure.Outbox;
 using Nexus.UserManagement.Service.Infrastructure.Persistence.Contexts;
 using Nexus.UserManagement.Service.Infrastructure.Persistence.Repositories.Users;
+using Shared.Test.Cache;
 using Xunit;
 
 namespace Nexus.UserManagement.Service.Integration.Tests.Handlers.Commands
@@ -20,8 +21,11 @@ namespace Nexus.UserManagement.Service.Integration.Tests.Handlers.Commands
         private readonly UserRepository _repo;
         private readonly IUnitOfWork _uow;
         private readonly OutboxSignal _outboxSignal;
+        private readonly InMemoryCacheService _cache;
         private readonly ChangeEmailCommandHandler _handler;
         private readonly CancellationToken _ct = default;
+
+        private string _code = "724014";
 
         public ChangeEmailCommandHandlerTests(TestFixture fixture)
         {
@@ -31,8 +35,9 @@ namespace Nexus.UserManagement.Service.Integration.Tests.Handlers.Commands
             _repo = new UserRepository(_context);
             _outboxSignal = (OutboxSignal)fixture.CreateOutboxSignal();
             _uow = fixture.CreateUnitOfWork(_context);
+            _cache = new InMemoryCacheService();
 
-            _handler = new ChangeEmailCommandHandler(_repo, _uow);
+            _handler = new ChangeEmailCommandHandler(_repo, _uow, _cache);
         }
 
         public async ValueTask InitializeAsync() => await _fixture.ResetDatabaseAsync();
@@ -59,7 +64,9 @@ namespace Nexus.UserManagement.Service.Integration.Tests.Handlers.Commands
             await _context.Users.AddAsync(user, _ct);
             await _context.SaveChangesAsync(_ct);
 
-            var command = new ChangeEmailCommand(user.Id, "new@email.com");
+            await _cache.SetStringAsync($"ConfirmCode for {user.Login.Value.ToLowerInvariant()}", _code, TimeSpan.FromMinutes(5));
+            
+            var command = new ChangeEmailCommand(user.Id, "new@email.com", _code);
 
             var result = await _handler.Handle(command, _ct);
 
@@ -78,7 +85,7 @@ namespace Nexus.UserManagement.Service.Integration.Tests.Handlers.Commands
         [Fact]
         public async Task Handle_NonExistingUserId_ShouldReturnFailureNotFoundCode()
         {
-            var command = new ChangeEmailCommand(Guid.NewGuid(), "new@email.com");
+            var command = new ChangeEmailCommand(Guid.NewGuid(), "new@email.com", _code);
             var result = await _handler.Handle(command, _ct);
             result.IsSuccess.Should().BeFalse();
             result.Errors.FirstOrDefault(e => e.Code == ErrorCode.NotFound)?.Code.Should().Be(ErrorCode.NotFound);
